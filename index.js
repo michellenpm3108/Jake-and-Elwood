3cfc6e6c964f03e17bc3abbf4629cc35d0a6629d
Michelle <michelle.npm3108@gmail.com>
2021-01-26 07:50:27 +0800

    add transform for nav side bar

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const exitBtn = document.getElementById('exit-btn')
-const menuBtn = document.getElementById('menu-btn')
-const navigation = document.getElementById('nav')
-menuBtn.addEventListener('click', function(){
-    navigation.style.display="block"
-})
-exitBtn.addEventListener('click', function(){
-    navigation.style.display="none"
-})
+const openNav = document.querySelector('.open-nav-btn')
